7368f8fc85359e87410f5c0659b2c2574bd9962f
Oswaldo <U22209742@utp.edu.pe>
2026-04-29 21:42:20 -0500

    feat: Added cachier layout

---
+++ app/layouts/cashier.vue
@@ -0,0 +1,58 @@
+<script lang="ts" setup>
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const open = ref(false)
+
+const links = [
+  {
+    label: 'Cajas',
+    icon: 'i-lucide-hand-coins',
+    to: '/cajas',
+    onSelect: () => {
+      open.value = false
+    }
+  }
+
+] as NavigationMenuItem[]
+</script>
+
+<template>
+  <UDashboardGroup unit="rem">
+    <UDashboardSidebar
+      id="default"
+      v-model:open="open"
+      collapsible
+      resizable
+      class="bg-elevated/25 transition-all duration-300"
+      :ui="{ footer: 'lg:border-t lg:border-default' }"
+    >
+      <template #header="{ collapsed }">
+        <!-- icon -->
+        <LogoTitle :show-title="!collapsed" />
+      </template>
+
+      <template #default="{ collapsed }">
+        <UNavigationMenu
+          :collapsed="collapsed"
+          :items="links"
+          orientation="vertical"
+          tooltip
+          popover
+        />
+
+        <UNavigationMenu
+          :collapsed="collapsed"
+          orientation="vertical"
+          tooltip
+          class="mt-auto"
+        />
+      </template>
+
+      <template #footer="{ collapsed }">
+        <UserMenu :collapsed="collapsed" />
+      </template>
+    </UDashboardSidebar>
+
+    <slot />
+  </UDashboardGroup>
+</template>
